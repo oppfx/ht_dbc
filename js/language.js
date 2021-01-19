da656e0c4e9e4f528d0022244b8bd8e70c44ab1e
joker <>
2021-01-19 20:07:16 +0800

    first commit

---
+++ js/language.js
@@ -0,0 +1,457 @@
+var indexhtml = {
+    home:{
+        'zh-cn':'首页',
+        'en-us':'Home'
+    },
+    add:{
+        'zh-cn':'添加',
+        'en-us':'Add'
+    },
+    banks:{
+        'zh-cn':'银行',
+        'en-us':'Banks'
+    },
+    bonds:{
+        'zh-cn':'债券',
+        'en-us':'Bonds'
+    },
+    boardroom:{
+        'zh-cn':'董事会',
+        'en-us':'Boardroom'
+    },
+    cordyceps:{
+        'zh-cn':'虫草',
+        'en-us':'Cordyceps'
+    },
+    referral:{
+        'zh-cn':'邀请',
+        'en-us':'Referral'
+    },
+    about:{
+        'zh-cn':'白皮书',
+        'en-us':'About'
+    },
+    contract:{
+        'zh-cn':'合约',
+        'en-us':'Contracts'
+    },
+    presale:{
+        'zh-cn':'预售',
+        'en-us':'Pre-Sale'
+    },
+    current:{
+        'zh-cn':'当前',
+        'en-us':'Current'
+    },
+    available:{
+        'zh-cn':'可使用',
+        'en-us':'Available'
+    },
+    purchase:{
+        'zh-cn':'购买',
+        'en-us':'Purchase'
+    },
+    
+    redeem:{
+        'zh-cn':'赎回',
+        'en-us':'Redeem'
+    },
+    myacc:{
+        'zh-cn':'我的账户',
+        'en-us':'My Account'
+    },
+    connect:{
+        'zh-cn':'连接',
+        'en-us':'Connect'
+    },
+    conwallet:{
+        'zh-cn':'连接钱包',
+        'en-us':'Connect Wallet'
+    },
+    epoch:{
+        'zh-cn':'纪元',
+        'en-us':'Epoch'
+    },
+    next:{
+        'zh-cn':'下一个',
+        'en-us':'Next'
+    },
+    scafactor:{
+        'zh-cn':'通胀比例',
+        'en-us':'Scaling Factor'
+    },
+    treabal:{
+        'zh-cn':'财政储备金',
+        'en-us':'Treasury Amount'
+    },
+    
+    price:{
+        'zh-cn':'价格',
+        'en-us':'Price'
+    },
+    price2:{
+        'zh-cn':'价格',
+        'en-us':' Price'
+    },   
+    hpt1:{
+        'zh-cn':'欢迎来到 DollarBalance Cash!',
+        'en-us':'Welcome to DollarBalance Cash!'
+    },
+    hpt2:{
+        'zh-cn':'在Mdex上购买，出售和添加DollarBalance Cash(DBC) 的流动性',
+        'en-us':'Buy,sell,and provide liquidity for DollarBalance Cash(DBC) on Mdex'
+    },
+    tsupply:{
+        'zh-cn':'总供应量',
+        'en-us':'Total Supply'
+    },
+    cliquidty:{
+        'zh-cn':'当前流通量',
+        'en-us':'Current Liquidity'
+    },
+    brt1:{
+        'zh-cn':'加入董事会',
+        'en-us':'Join the Boardroom'
+    },
+    brt2:{
+        'zh-cn':'存入DollarBalance Shares (DBS)，赚取通胀回报',
+        'en-us':'Deposit DollarBalance Shares (DBS) and earn inflationary rewards'
+    },
+    bpt1:{
+        'zh-cn':'购买&赎回债券',
+        'en-us':'Buy&Redeem Bonds'
+    },
+    bpt2:{
+        'zh-cn':'赎回时赚取保费',
+        'en-us':'Earn premiums upon redemption'
+    },
+    fpt1:{
+        'zh-cn':'选择一家银行',
+        'en-us':'Pick a Bank.'
+    },
+    fpt2:{
+        'zh-cn':'通过添加流动性获得DollarBalance Cash (DBC)',
+        'en-us':'Earn DollarBalance Cash (DBC) by providing liquidity'
+    },
+    fpt3:{
+        'zh-cn':'请输入数量',
+        'en-us':'Please enter amount'
+    },
+    max:{
+        'zh-cn':'最大',
+        'en-us':'Max'
+    },
+    subscri:{
+        'zh-cn':'　认购　',
+        'en-us':'Subscripe'
+    },
+    pst1:{
+        'zh-cn':'DollarBalance Share (DBS)为项目股份，持有DBS股份用户可以直接享受DBC通胀分成(25%)，每个通胀纪元都可以获取分红，总量500000，不可增发，合约已开源',
+        'en-us':'DollarBalance Share (DBS) is a project share. Users who hold DBS shares can directly enjoy the DBC inflation share (25%).Every inflation epoch can get dividends. The total supply is 500000. Never issue more, and the contract is open source.'
+    },
+    pst2:{
+        'zh-cn':'预售数量',
+        'en-us':'Pre-sale Quantity'
+    },
+    pst3:{
+        'zh-cn':'预售价格',
+        'en-us':'Pre-sale Price'
+    },
+    pst4:{
+        'zh-cn':'预售进度',
+        'en-us':'Pre-sale Progress'
+    },
+    psmt1:{
+        'zh-cn':'确认额度',
+        'en-us':'Subscription Limit'
+    },
+    ok:{
+        'zh-cn':'确定',
+        'en-us':'OK'
+    },
+    
+
+    clarew:{
+        'zh-cn':'领取奖励',
+        'en-us':'Claim Reward'
+    },
+    bmpt1:{
+        'zh-cn':'即可获取 DollarBalance Cash (DBC)',
+        'en-us':'and earn DollarBalance Cash (DBC)'
+    },
+    bmpt2:{
+        'zh-cn':'当前纪元奖励',
+        'en-us':'Current Epoch Reward'
+    },
+  
+    settle:{
+        'zh-cn':'结算',
+        'en-us':'Settle'
+    },
+    stake:{
+        'zh-cn':'质押',
+        'en-us':'Stake'
+    },
+    stake2:{
+        'zh-cn':'质押',
+        'en-us':' Stake'
+    },
+    staked:{
+        'zh-cn':'已质押',
+        'en-us':'Staked'
+    },
+
+    earned:{
+        'zh-cn':'已获取',
+        'en-us':'Earned'
+    },
+    install:{
+        'zh-cn':'安装',
+        'en-us':'Install'
+    },
+   
+    reflink:{
+        'zh-cn':'邀请链接',
+        'en-us':'Refer Link'
+    },
+    rmt1:{
+        'zh-cn':'邀请链接 (获得用户银行结算奖励的5%)',
+        'en-us':'Refer Link (5% of bank settlement as reward)'
+    },
+    rmt2:{
+        'zh-cn':'获取ID失败，请先注册',
+        'en-us':'Failed to get ID, please register first'
+    },
+    copy:{
+        'zh-cn':'复制',
+        'en-us':'Copy'
+    },
+    balance:{
+        'zh-cn':'余额',
+        'en-us':'Balance'
+    },
+    regis:{
+        'zh-cn':'　注册ID　',
+        'en-us':'Register'
+    },
+    refbon:{
+        'zh-cn':'邀请奖励',
+        'en-us':'Refer Rewards'
+    },
+    refnum:{
+        'zh-cn':'你已邀请:',
+        'en-us':'Referrals:'
+    },
+    withdraw:{
+        'zh-cn':'　提现　',
+        'en-us':'Withdraw'
+    },
+    dbcbal:{
+        'zh-cn':'DBC余额',
+        'en-us':'DBC Balance'
+    },
+    dbbbal:{
+        'zh-cn':'DBB余额',
+        'en-us':'DBB Balance'
+    },
+    dbsbal:{
+        'zh-cn':'DBS余额',
+        'en-us':'DBS Balance'
+    },
+    close:{
+        'zh-cn':'　关闭　',
+        'en-us':'Close'
+    },
+    closek:{
+        'zh-cn':'　关闭　',
+        'en-us':'　Close　'
+    },  
+    
+    prompt:{
+        'zh-cn':'提示',
+        'en-us':'Prompt'
+    }
+  }
+  
+
+
+
+  var appjs ={
+    version:{
+        'zh-cn':'1.02',
+        'en-us':'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890.,/#:：'
+    },
+    weight:{
+        'zh-cn':'权重',
+        'en-us':'Weight'
+    },
+    neterr:{
+        'zh-cn':'网络错误',
+        'en-us':'Network Error'
+    },
+    buy:{
+        'zh-cn':'购买',
+        'en-us':'Purchase'
+    },
+    priover:{
+        'zh-cn':'价格超过',
+        'en-us':'is over'
+    },
+    priunder:{
+        'zh-cn':'价格低于',
+        'en-us':'is under'
+    },
+    redeem:{
+        'zh-cn':'赎回',
+        'en-us':'Redeem'
+    },
+    regis1:{
+        'zh-cn':'注册中...',
+        'en-us':'Registering...'
+    },
+    regis2:{
+        'zh-cn':'注册成功!',
+        'en-us':'Register success!'
+    },
+    regisfail:{
+        'zh-cn':'注册失败!',
+        'en-us':'Register fail!'
+    },
+    copy:{
+        'zh-cn':'复制',
+        'en-us':'Copy'
+    },
+    copysucc:{
+        'zh-cn':'复制成功',
+        'en-us':'Copy success'
+    },
+    copyfail:{
+        'zh-cn':'复制失败',
+        'en-us':'Copy fail'
+    },
+    isunder:{
+        'zh-cn':'DBC>$1时回报率为:',
+        'en-us':'return when DBC > $1'
+    },
+    isover:{
+        'zh-cn':'DBC 价格高于 $1',
+        'en-us':'DBC is over $1'
+    },
+    redunder:{
+        'zh-cn':'DBC > $1.05 时可用',
+        'en-us':'Enabled when DBC > $1.05'
+    },
+    
+    select:{
+        'zh-cn':'　选择　',
+        'en-us':'Select'
+    },
+    end:{
+        'zh-cn':'结束',
+        'en-us':'Ended'
+    },
+    earn:{
+        'zh-cn':'获取',
+        'en-us':'Earn'
+    },
+    staket:{
+        'zh-cn':'质押',
+        'en-us':'Stake'
+    },
+    verifyt:{
+        'zh-cn':'验证',
+        'en-us':'Verify'
+    },
+    stake:{
+        'zh-cn':'　质押　',
+        'en-us':'Stake'
+    },
+    comson:{
+        'zh-cn':'即将开始',
+        'en-us':'Coming soon'
+    },
+
+    staked:{
+        'zh-cn':'已质押',
+        'en-us':'staked'
+    },
+    wait:{
+        'zh-cn':'等待',
+        'en-us':'Wait'
+    },
+    succ:{
+        'zh-cn':'　成功　',
+        'en-us':'Success'
+    },
+    close:{
+        'zh-cn':'关闭',
+        'en-us':'Close'
+    },
+    
+  }
+
+
+  
+  var i18nData={
+    indexhtml:indexhtml,
+    appjs:appjs
+  }
+  var I18N = {
+    DATA : i18nData,
+    language: 'zh-cn',
+    setLanguage: function(language){
+        this.language = language;
+    }
+
+    ,parse: function(str, pageName){
+        var _this = this;
+        if(!pageName){
+            console.error('I18N error: pageName='+pageName);
+            return;
+        }
+        if(!_this.language){
+            console.error('I18N error: language='+_this.language);
+            return;
+        }
+        var key= '',
+        parseStr = '';
+        parseStr = str.replace(/{{i18n-(.+?)}}/g, function(t){
+            key = t.slice(7, t.length - 2);
+            try{
+                return _this.DATA[pageName][key][_this.language] || '';
+            }catch (e){
+                console.warn('language not found, key:'+ key +'----val'+ _this.language );
+                return '';
+            }
+        });
+        return parseStr;
+    }
+   
+    ,getText: function(pageName, key, v1, v2, v3){
+        var _this = this;
+        var intrusion = [];
+        if(arguments.length == 3 && typeof(arguments[2]) == 'object'){
+            intrusion = arguments[2];
+        }
+        else if(arguments.length > 2){
+            for(var i=2; i< arguments.length; i++){
+            intrusion.push(arguments[i]);
+            }
+        }
+        var _lg = '';
+        try{
+            _lg = _this.DATA[pageName][key][_this.language] || '';
+            if(!intrusion || intrusion.length == 0){
+                return _lg;
+            }
+            _lg = _lg.replace(/{d+}/g, function( word ){
+                return intrusion[word.match(/d+/)];
+            });
+            return _lg;
+        }catch (e){
+            console.warn('language not found, key:'+ key +'----val'+ _this.language );
+            return '';
+        }
+    }
+  };
+  
